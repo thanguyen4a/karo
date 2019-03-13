a0d0bc26ebf673d42b06804bad4e647d39227a92
Nguyen Tha <NguyenTha@gnavi.co.jp>
2019-03-13 19:49:30 +0900

    second

--- web/js/ultil.js
+++ web/js/ultil.js
@@ -1,3 +1,7 @@
+var cell_number = 10;
+var win_lenght = 5;
+
+
 var PLAYERS = {
     NONE: 0,
     NO_1: 1,
@@ -20,16 +24,15 @@ var PLAYER_TICK = {
 
 };
 
-const cell_number = 10;
 var arr = [];
 var state;
 var winer;
 
 
 $(document).ready(function () {
+
     //init
     init();
-
 
     var body = document.getElementsByTagName("body")[0];
 
@@ -37,27 +40,15 @@ $(document).ready(function () {
     var tblB = document.createElement("TBODY");
     table.appendChild(tblB);
 
-
-
-    for (var i = 0; i < cell_number; i++) {
-        var TR = document.createElement("TR");
-        tblB.appendChild(TR);
-        for (var j = 0; j < cell_number; j++) {
-            var TD = document.createElement("TD");
-            TD.setAttribute("id", i + "" + j);
-            TD.setAttribute("class","height-30 width-30 background-color-00ffbf border-bisque text-align");
-            var text = document.createTextNode(PLAYER_TICK.NONE);
-            TD.appendChild(text);
-            TR.appendChild(TD);
-        }
-    }
+    generateTable(tblB);
     body.appendChild(table);
 
     $("TD").click(function () {
 
         var selector = $(this);
         var id_str = selector.attr("id");
-        var result = TickCell(id_str, selector);
+        var span = selector.children('span');
+        var result = process(id_str, span);
     }
     );
 }
@@ -74,38 +65,76 @@ function Create2DArray() {
     for (var i = 0; i < cell_number; i++) {
         this.arr[i] = new Array(cell_number);
         for (var j = 0; j < cell_number; j++) {
-            this.arr[i][j] = {player: PLAYERS.NONE};
+            this.arr[i][j] = {player: PLAYERS.NONE, id: "" + i + j};
         }
     }
 }
 
-function TickCell(id_str, selector) {
+
+function generateTable(tblB) {
+    for (var i = 0; i < cell_number; i++) {
+        var TR = document.createElement("TR");
+        tblB.appendChild(TR);
+        for (var j = 0; j < cell_number; j++) {
+            var TD = document.createElement("TD");
+            TD.setAttribute("id", i + "" + j);
+            TD.setAttribute("class", "height-30 width-30 background-color-00ffbf border-bisque text-align-center");
+            var text = document.createTextNode(PLAYER_TICK.NONE);
+            var span  = document.createElement("span");
+            span.appendChild(text);
+            
+            TD.appendChild(span);
+            TR.appendChild(TD);
+        }
+    }
+    
+}
+
+
+
+function process(id_str, selector) {
     var result = false;
+    var player = PLAYERS.NONE;
     switch (this.state) {
         case STATE.WATTING:
         case STATE.FINISHED:
             break;
         case STATE.NO_1:
-            result = SetData(id_str, PLAYERS.NO_1);
-            if (result) {
-                selector.html(PLAYER_TICK.NO_1);
-            }
+            player = PLAYERS.NO_1;
+            result = setData(id_str, player);
             break;
         case STATE.NO_2:
-            result = SetData(id_str, PLAYERS.NO_2);
-            if (result) {
-                selector.html(PLAYER_TICK.NO_2);
-            }
+            player = PLAYERS.NO_2;
+            result = setData(id_str, player);
             break;
     }
-    
-    if(result) {
-        nextTurn();
+
+    if (result) {
+        tickXO(selector, player);
+        var sub_array = determineWinner(id_str, player);
+        if (sub_array instanceof Array) {
+            console.log("thang roi");
+            this.state = STATE.FINISHED;
+            printID(sub_array)
+        } else {
+            console.log("chua thang");
+            nextTurn();
+        }
+
     }
     return result;
 }
 
-function SetData(id_str, player) {
+function tickXO(selector, player)
+{
+    if (player === PLAYERS.NO_1) {
+        selector.html(PLAYER_TICK.NO_1);
+    } else if (player === PLAYERS.NO_2) {
+        selector.html(PLAYER_TICK.NO_2);
+    }
+}
+
+function setData(id_str, player) {
     var i = parseInt(id_str.substring(0, 1));
     var j = parseInt(id_str.substring(1, 2));
     var tmp = this.arr[i][j];
@@ -129,4 +158,205 @@ function nextTurn()
 
 }
 
+function determineWinner(id_str, player) {
+    return determineHorizontalWin(id_str, player);
+}
 
+function determineHorizontalWin(id_str, player)
+{
+    var i = parseInt(id_str.substring(0, 1));
+    var j = parseInt(id_str.substring(1, 2));
+
+    for (var delta = 0; delta < win_lenght; delta++) {
+//        console.log("j - delta" + (j - delta));
+//         console.log(" j + (win_lenght - delta - 1) " + ( j + (win_lenght - delta - 1) ));
+        if (j - delta < 0 || j + (win_lenght - delta - 1) >= cell_number) {
+            continue;
+        }
+        var sub_array = isWinSubHorizontalArray(i, j - delta, win_lenght, player)
+        if (sub_array != false) {
+            return sub_array;
+        }
+    }
+
+    return determineVerticalWin(id_str, player);
+}
+
+function isWinSubHorizontalArray(i, start, end, player)
+{
+    var sub_array = new Array(win_lenght);
+    for (var j = start; j < start + end; j++)
+    {
+        sub_array[j - start] = this.arr[i][j];
+        if (this.arr[i][j]["player"] != player) {
+            return false;
+        }
+    }
+    return sub_array;
+}
+
+
+function determineVerticalWin(id_str, player)
+{
+    var i = parseInt(id_str.substring(0, 1));
+    var j = parseInt(id_str.substring(1, 2));
+
+    for (var delta = 0; delta < win_lenght; delta++) {
+//        console.log("j - delta" + (j - delta));
+//         console.log(" j + (win_lenght - delta - 1) " + ( j + (win_lenght - delta - 1) ));
+        if (i - delta < 0 || i + (win_lenght - delta - 1) >= cell_number) {
+            continue;
+        }
+        var sub_array = isWinSubVerticalArray(j, i - delta, win_lenght, player)
+        if (sub_array != false) {
+            return sub_array;
+        }
+    }
+
+    return determineCrossFirstWin(id_str, player);
+}
+
+function isWinSubVerticalArray(j, start, end, player)
+{
+    var sub_array = new Array(win_lenght);
+    for (var i = start; i < start + end; i++)
+    {
+        sub_array[i - start] = this.arr[i][j];
+        if (this.arr[i][j]["player"] != player) {
+            return false;
+        }
+    }
+    return sub_array;
+}
+
+
+
+
+function determineCrossFirstWin(id_str, player)
+{
+    var i = parseInt(id_str.substring(0, 1));
+    var j = parseInt(id_str.substring(1, 2));
+
+    for (var delta = 0; delta < win_lenght; delta++) {
+//        console.log("j - delta" + (j - delta));
+//         console.log(" j + (win_lenght - delta - 1) " + ( j + (win_lenght - delta - 1) ));
+        if (i - delta < 0 || i + (win_lenght - delta - 1) >= cell_number) {
+            continue;
+        }
+        
+        if (j - delta < 0 || j + (win_lenght - delta - 1) >= cell_number) {
+            continue;
+        }
+        
+        
+        var sub_array = isWinSubCrossFirstArray(i,j,delta, player)
+        if (sub_array != false) {
+            return sub_array;
+        }
+    }
+
+    return determineCrossSecondWin(id_str, player);
+}
+
+function isWinSubCrossFirstArray(i,j, start, player)
+{
+    var sub_array = new Array(win_lenght);
+    for (var m = -start; m < -start + win_lenght; m++)
+    {
+        sub_array[m + start] = this.arr[i+m][j+m];
+        if (this.arr[i+m][j+m]["player"] != player) {
+            return false;
+        }
+    }
+    return sub_array;
+}
+
+
+
+
+function determineCrossSecondWin(id_str, player)
+{
+    var i = parseInt(id_str.substring(0, 1));
+    var j = parseInt(id_str.substring(1, 2));
+
+    for (var delta = 0; delta < win_lenght; delta++) {
+//        console.log("j - delta" + (j - delta));
+//         console.log(" j + (win_lenght - delta - 1) " + ( j + (win_lenght - delta - 1) ));
+        if (i - delta < 0 || i + (win_lenght - delta - 1) >= cell_number) {
+            continue;
+        }
+        
+        if (j - delta < 0 || j + (win_lenght - delta - 1) >= cell_number) {
+            continue;
+        }
+        
+        
+        var sub_array = isWinSubCrossSecondArray(i,j,delta, player)
+        if (sub_array != false) {
+            return sub_array;
+        }
+    }
+
+    return false;
+}
+
+function isWinSubCrossSecondArray(i,j, start, player)
+{
+    var sub_array = new Array(win_lenght);
+    for (var m = -start; m < -start + win_lenght; m++)
+    {
+        sub_array[m + start] = this.arr[i-m][j+m];
+        if (this.arr[i-m][j+m]["player"] != player) {
+            return false;
+        }
+    }
+    return sub_array;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function printID(sub_array) {
+    var id_list = "";
+    for (var i = 0; i < win_lenght; i++) {
+        var id = sub_array[i]["id"];
+        console.log("id" + id);
+        id_list += "#" + id;
+        if (i != win_lenght - 1) {
+            id_list += ",";
+        }
+    }
+    console.log("id_list" + id_list);
+    console.log("class " +  $("#65").attr("class"));
+    fadeToogle(id_list);
+
+}
+
+
+function fadeToogle(id_list)
+{
+//    var elements = $(id_list);
+//    setInterval(function () {
+            $(id_list).each(function () {
+                     console.log("id ... ddddddddddddddddddddd" +  $(this).attr("class"));
+                     $(this).addClass("text-color-red-italic"); 
+                 }) ;
+//                 }, 4000);
+
+//    });
+
+}
