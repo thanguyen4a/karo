4c4e94b42257e7459748ac7cc972bcffbe48ca39
Nguyen Tha <NguyenTha@gnavi.co.jp>
2019-03-22 11:40:03 +0900

    xong phan can chinh css

--- web/js/form.js
+++ web/js/form.js
@@ -6,10 +6,27 @@
 
 var max_lenght_user = 10;
 var max_lenght_age = 100;
+var max_capital = 1000;  //byte
 
 
 
 $(document).ready(function () {
+
+    avatar = document.getElementById("avatar");
+    var file;
+
+    avatar.addEventListener("change", function (evt) {
+        console.log("mmm");
+        file = evt.target.files;
+
+
+        if (file === null) {
+
+            console.log("avatar null");
+        } else {
+            console.log("khac null");
+        }
+    });
 
     $("#submit").click(function () {
         var user = $("#user").val();
@@ -18,7 +35,7 @@ $(document).ready(function () {
         var tel = $("#tel").val();
         var mail = $("#mail").val();
         var birthday = $("#birthday").val();
-        var avatar = $("#avatar").val();
+
         var error = $("#error");
 
 
@@ -28,6 +45,8 @@ $(document).ready(function () {
         errors += validateSex(sex);
         errors += validateMail(mail);
         errors += validateTel(tel);
+        errors += validateDate(birthday);
+        errors += validateAvatar(file);
         error.html(errors);
     }
     );
@@ -37,73 +56,85 @@ $(document).ready(function () {
 
 
 function validateUser(user) {
-    var re = new RegExp("^[a-zA-Z0-9]*$");
+    var re = new RegExp("^[a-zA-Z0-9]+[a-zA-Z0-9]*$");
     if (!re.test(user))
-        return "User name is invalid \n"; 
-    if(user.length > max_lenght_user)
-        return "User name must be less than or equal to"+ max_lenght_user +"letters\n";
-    
+        return "User name is invalid \n";
+    if (user.length > max_lenght_user)
+        return "User name must be less than or equal to" + max_lenght_user + "letters\n";
+
     return "";
 }
 
 function validateAge(age) {
-    var re = new RegExp("^[\d]*$");
+    var re = new RegExp("^[\d]+[\d]*$");
     if (!re.test(age))
-        return "Age is invalid \n"; 
+        return "Age is invalid \n";
     var age_number = parseInt(age);
-    if(age_number > max_lenght_age || age_number < 0)
-        return "Age must be beetween from 0 to "+ max_lenght_age + "\n";
-    
+    if (age_number > max_lenght_age || age_number < 0)
+        return "Age must be beetween from 0 to " + max_lenght_age + "\n";
+
     return "";
 }
 
 
-function validateSex(sex) {   
-    if(sex.length === 0)
+function validateSex(sex) {
+    if (sex.length === 0)
         return "Sex is invalid \n";
     return "";
 }
 
-function validateMail(mail) {   
+function validateMail(mail) {
     var regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     var re = new RegExp(regexp);
     if (!re.test(mail))
-        return "Mail is invalid \n"; 
+        return "Mail is invalid \n";
     return "";
 }
 
 
-function validateTel(tel) {   
+function validateTel(tel) {
     console.log("tel " + tel);
     var regexp = "^(?:\\d{11}|\\d{3}\-\\d{4}\-\\d{4}|\\d{3} \\d{4} \\d{4})$";
     var re = new RegExp(regexp);
     if (!re.test(tel))
-        return "Tel is invalid \n"; 
-    return "";
-}
-
-function validateDate(date) {   
-    console.log("tel " + date);
-    var regexp = "^(?:\\d{11}|\\d{3}\-\\d{4}\-\\d{4}|\\d{3} \\d{4} \\d{4})$";
-    var re = new RegExp(regexp);
-    if (!re.test(tel))
-        return "Tel is invalid \n"; 
+        return "Tel is invalid \n";
     return "";
 }
 
 
-function isYYYYMMDD(str){
-  //null or 8文字でない or 数値でない場合はfalse
-  if(str==null || str.length != 8 || isNaN(str)){
-    return false;
-  }
- 
-  //年,月,日を取得する
-  var y = parseInt(str.substr(0,4));
-  var m = parseInt(str.substr(4,2)) ;
-  var d = parseInt(str.substr(6,2));
-  var dt = new Date(y, m, d);
+//  YYYY/MM/DD
+function validateDate(date) {
+    console.log("date " + date);
 
-  return (y == dt.getFullYear() && m == dt.getMonth() && d == dt.getDate());
+    var m1 = moment(date);
+
+    if (m1.isValid()) {
+        return "";
+    } else {
+        return "Date is invalid \n";
+    }
+}
+
+
+function validateAvatar(file) {
+//    console.log("avatar " + avatar);
+    if (!file) {
+        return "Avatar is null \n";
+    }
+
+    var types = ['text/plain', 'image/png'];
+    console.log("file.type " + file[0].type);
+    console.log("file.size " + file[0].size);
+    if (!types.includes(file[0].type)) {
+        return "Avatar must be text or png file";
+    }
+    if (file[0].size >= max_capital) {
+        return "Avatar capital must be less than or equal to " + max_capital + "byte";
+    }
+
+
+    
+     return "";
+
 }
